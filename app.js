101596422ff2e16c39c0be05897226c2d413b6cd
Rishabh PC <rishabhsinha220@gmail.com>
2020-08-27 18:30:23 +0530

    Added some of the codes

---
+++ app.js
@@ -0,0 +1,18 @@
+const game = () => {
+  let pScore = 0;
+  let cScore = 0;
+
+  const startGame = () => {
+    const playButton = document.querySelector(".intro button");
+    const introScreen = document.querySelector(".intro");
+    const match = document.querySelector(".match");
+
+    playButton.addEventListener("click", () => {
+      introScreen.classList.add("fadeOut");
+      match.classList.add("fadeIn");
+    });
+  };
+  startGame();
+};
+
+game();
